7c0f56476111a1666dee3676c8b6e47d804ccaca
Emanorion <nextgengamer.prime@gmail.com>
2025-06-26 15:53:27 +0200

    :wheelchair:

--- project-module/src/components/SideBar.vue
+++ project-module/src/components/SideBar.vue
@@ -1,41 +1,63 @@
 <template>
     <div class="sideBar">
         <div class="sidebar-title">
-            <p><i class="fa fa-fw fa-home"></i>Dashboard</p>
+            <p>Dashboard</p>
         </div>
         <div class="items">
-            <p class="icon"><router-link to="/payroll"><i class="fa fa-credit-card"></i>Payroll</router-link></p>
-            <p class="icon"><router-link to="/Employees"><i class="fa fa-user" aria-hidden="true"></i> Employees</router-link></p>
+            <ul>
+                    <li class="icon sidebar-item"><i class="fa fa-credit-card"></i><router-link to="/payroll"class="dash-link">Payroll</router-link></li>
+                    <li class="icon sidebar-item"><i class="fa fa-user" aria-hidden="true"></i><router-link to="/Employees"
+                        class="dash-link">Employees</router-link></li>
+                    <li class="icon sidebar-item"><i class="fa fa-bar-chart" aria-hidden="true"></i><router-link to="/overview"
+                        class="dash-link">Overview</router-link></li>
+                    <li class="icon sidebar-item"><i class="fa fa-tasks" aria-hidden="true"></i><router-link
+                        to="/overallattendance" class="dash-link">Attendance</router-link></li>
+                    <li class="icon sidebar-item"><i class="fa fa-calendar-o" aria-hidden="true"></i><router-link to="/leaveform"
+                        class="dash-link">Leave Requests</router-link></li>
+                    <li class="icon sidebar-item"><i class="fa fa-hand-o-left" aria-hidden="true"></i><router-link to="/"
+                        class="dash-link">Log Out</router-link></li>
+            </ul>
         </div>
     </div>
 
 </template>
 
 <script>
-import router from '@/router';
-
 export default {
     name: 'Dashboard'
 }
 </script>
 <style>
-.fa{
-    color: aquamarine;margin-right: 5px;
-
+.fa {
+    color: white;
 }
+
 .sidebar-title {
     font-size: 25px;
-    text-align: center;
+    margin-left: 20px;
+    margin-top: 20px;
+    margin-bottom: 50px;
+    color: skyblue;
+}
+
+.items ul {
+    list-style-type: none;
+    padding-left: 0;
+    margin: 0;
+}
+.items-box{
+    display: flex;
+}
+.dash-link {
+    color: white;
+    text-decoration: none;
 }
 
 .items {
     color: white;
     font-size: 20px;
     text-align: start;
-    width: 150px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
+    width: 200px;
 }
 
 .dashItems {
@@ -46,11 +68,18 @@ export default {
 .sideBar {
     background-color: #333;
     width: 200px;
-    height: 100vh;
+    height: 100%;
     z-index: 1000;
     position: fixed;
     overflow-y: auto;
     top: 0;
     left: 0;
+}
+.sidebar-item{
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    margin-left: 20px;
+    margin-top: 20px;
 }
 </style>
